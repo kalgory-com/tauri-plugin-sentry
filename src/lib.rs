04bc52b726bcb7ba02a1d1accd750bb8b5e0fd41
趙子賢 <danny900714@gmail.com>
2024-07-18 17:11:12 +0800

    Format code using cargo fmt

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 use tauri::{
-  plugin::{Builder, TauriPlugin},
-  Manager, Runtime,
+    plugin::{Builder, TauriPlugin},
+    Manager, Runtime,
 };
 
 pub use models::*;
@@ -23,26 +23,26 @@ use mobile::Sentry;
 
 /// Extensions to [`tauri::App`], [`tauri::AppHandle`] and [`tauri::Window`] to access the sentry APIs.
 pub trait SentryExt<R: Runtime> {
-  fn sentry(&self) -> &Sentry<R>;
+    fn sentry(&self) -> &Sentry<R>;
 }
 
 impl<R: Runtime, T: Manager<R>> crate::SentryExt<R> for T {
-  fn sentry(&self) -> &Sentry<R> {
-    self.state::<Sentry<R>>().inner()
-  }
+    fn sentry(&self) -> &Sentry<R> {
+        self.state::<Sentry<R>>().inner()
+    }
 }
 
 /// Initializes the plugin.
 pub fn init<R: Runtime>() -> TauriPlugin<R> {
-  Builder::new("sentry")
-    .invoke_handler(tauri::generate_handler![commands::ping])
-    .setup(|app, api| {
-      #[cfg(mobile)]
-      let sentry = mobile::init(app, api)?;
-      #[cfg(desktop)]
-      let sentry = desktop::init(app, api)?;
-      app.manage(sentry);
-      Ok(())
-    })
-    .build()
+    Builder::new("sentry")
+        .invoke_handler(tauri::generate_handler![commands::ping])
+        .setup(|app, api| {
+            #[cfg(mobile)]
+            let sentry = mobile::init(app, api)?;
+            #[cfg(desktop)]
+            let sentry = desktop::init(app, api)?;
+            app.manage(sentry);
+            Ok(())
+        })
+        .build()
 }
