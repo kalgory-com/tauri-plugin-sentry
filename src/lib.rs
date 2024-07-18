668764c6933b4f749aa5041a549395f071adabee
趙子賢 <danny900714@gmail.com>
2024-07-18 19:11:20 +0800

    Remove mobile support and update package information

--- src/lib.rs
+++ src/lib.rs
@@ -5,10 +5,7 @@ use tauri::{
 
 pub use models::*;
 
-#[cfg(desktop)]
-mod desktop;
-#[cfg(mobile)]
-mod mobile;
+mod api;
 
 mod commands;
 mod error;
@@ -16,17 +13,14 @@ mod models;
 
 pub use error::{Error, Result};
 
-#[cfg(desktop)]
-use desktop::Sentry;
-#[cfg(mobile)]
-use mobile::Sentry;
+use api::Sentry;
 
 /// Extensions to [`tauri::App`], [`tauri::AppHandle`] and [`tauri::Window`] to access the sentry APIs.
 pub trait SentryExt<R: Runtime> {
     fn sentry(&self) -> &Sentry<R>;
 }
 
-impl<R: Runtime, T: Manager<R>> crate::SentryExt<R> for T {
+impl<R: Runtime, T: Manager<R>> SentryExt<R> for T {
     fn sentry(&self) -> &Sentry<R> {
         self.state::<Sentry<R>>().inner()
     }
@@ -37,10 +31,8 @@ pub fn init<R: Runtime>() -> TauriPlugin<R> {
     Builder::new("sentry")
         .invoke_handler(tauri::generate_handler![commands::ping])
         .setup(|app, api| {
-            #[cfg(mobile)]
-            let sentry = mobile::init(app, api)?;
             #[cfg(desktop)]
-            let sentry = desktop::init(app, api)?;
+            let sentry = api::init(app, api)?;
             app.manage(sentry);
             Ok(())
         })
